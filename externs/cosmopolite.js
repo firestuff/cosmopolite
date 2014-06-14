a51359068f34d27b8aeaf3a08c66425322334619
Ian Gulliver <ian@isinglasstheater.org>
2014-06-14 12:21:16 -0700

    Flesh out externs file

--- externs/cosmopolite.js
+++ externs/cosmopolite.js
@@ -40,7 +40,10 @@ Cosmopolite.typeMessage;
 Cosmopolite.typeSubject;
 
 
-/** @typedef {(Cosmopolite.typeSubject|string|number)} */
+/**
+ * @see https://www.cosmopolite.org/reference#subject
+ * @typedef {(Cosmopolite.typeSubject|string|number)}
+ */
 Cosmopolite.typeSubjectLoose;
 
 
@@ -59,6 +62,28 @@ function Cosmopolite(
 
 
 /**
+ * @see https://www.cosmopolite.org/reference#shutdown
+ */
+Cosmopolite.prototype.shutdown = function() {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#getProfile
+ * @return {Promise}
+ * @nosideeffects
+ */
+Cosmopolite.prototype.getProfile = function() {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#currentProfile
+ * @const
+ * @nosideeffects
+ */
+Cosmopolite.prototype.currentProfile = function() {};
+
+
+/**
  * @see https://www.cosmopolite.org/reference#subscribe
  * @param {Cosmopolite.typeSubjectLoose|Array.<Cosmopolite.typeSubjectLoose>}
  *     subjects
@@ -68,3 +93,83 @@ function Cosmopolite(
  */
 Cosmopolite.prototype.subscribe =
   function(subjects, opt_messages, opt_lastID) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#unsubscribe
+ * @param {Cosmopolite.typeSubjectLoose} subject
+ * @return {Promise}
+ */
+Cosmopolite.prototype.unsubscribe = function(subject) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#sendMessage
+ * @param {Cosmopolite.typeSubjectLoose} subject
+ * @param {!*} message
+ * @return {Promise}
+ */
+Cosmopolite.prototype.sendMessage = function(subject, message) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#getMessages
+ * @param {Cosmopolite.typeSubjectLoose} subject
+ * @return {Array.<Cosmopolite.typeMessage>}
+ * @const
+ * @nosideeffects
+ */
+Cosmopolite.prototype.getMessages = function(subject) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#getLastMessage
+ * @param {Cosmopolite.typeSubjectLoose} subject
+ * @return {?Cosmopolite.typeMessage}
+ * @const
+ * @nosideeffects
+ */
+Cosmopolite.prototype.getLastMessage = function(subject) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#pin_method
+ * @param {Cosmopolite.typeSubjectLoose} subject
+ * @param {!*} message
+ * @return {Promise}
+ */
+Cosmopolite.prototype.pin = function(subject, message) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#unpin
+ * @param {string} id
+ * @return {Promise}
+ */
+Cosmopolite.prototype.unpin = function(id) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#getPins
+ * @param {Cosmopolite.typeSubjectLoose} subject
+ * @return {Array.<Cosmopolite.typeMessage>}
+ * @const
+ * @nosideeffects
+ */
+Cosmopolite.prototype.getPins = function(subject) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#trackEvent
+ * @param {...*} var_args
+ */
+Cosmopolite.prototype.trackEvent = function(var_args) {};
+
+
+/**
+ * @see https://www.cosmopolite.org/reference#uuid
+ * @return {string}
+ * @const
+ * @nosideeffects
+ */
+Cosmopolite.prototype.uuid = function() {};
