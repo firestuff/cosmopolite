761cb9276748e78952772cd1c01509f92e7f1c1d
Ian Gulliver <ian@flamingcow.tv>
2015-05-31 18:33:31 -0700

    Doc updates for the change in event handling framework.

--- externs/cosmopolite.js
+++ externs/cosmopolite.js
@@ -16,19 +16,6 @@ String.prototype.hashCode = function() {};
 
 
 /**
- * @see https://www.cosmopolite.org/reference#callbacks
- * @typedef {{onConnect: (function()|undefined),
- *            onDisconnect: (function()|undefined),
- *            onLogin: (function(string, string)|undefined),
- *            onLogout: (function(string)|undefined),
- *            onMessage: (function(Cosmopolite.typeMessage)|undefined),
- *            onPin: (function(Cosmopolite.typeMessage)|undefined),
- *            onUnpin: (function(Cosmopolite.typeMessage)|undefined)}}
- */
-Cosmopolite.typeCallbacks;
-
-
-/**
  * @see https://www.cosmopolite.org/reference#message
  * @typedef {{event_type: string,
  *            id: number,
@@ -60,14 +47,12 @@ Cosmopolite.typeSubjectLoose;
 /**
  * @see https://www.cosmopolite.org/reference#constructor
  * @constructor
- * @param {?Cosmopolite.typeCallbacks=} opt_callbacks
  * @param {?string=} opt_urlPrefix
  * @param {?string=} opt_namespace
  * @param {?string=} opt_trackingID
  * @nosideeffects
  */
-function Cosmopolite(
-    opt_callbacks, opt_urlPrefix, opt_namespace, opt_trackingID) {}
+function Cosmopolite(opt_urlPrefix, opt_namespace, opt_trackingID) {}
 
 
 /**
