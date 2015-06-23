f3bde736f768ccd500cd45e5973d59f619d8a0d0
Ian Gulliver <flamingcow@gmail.com>
2015-06-23 03:45:00 +0000

    Log timestamp of torture run start.

--- clients/c/torture.sh
+++ clients/c/torture.sh
@@ -6,5 +6,6 @@ export COSMO_DEBUG=
 make test
 
 while :; do
-	valgrind --leak-check=full --show-reachable=yes --num-callers=20 --suppressions=suppressions ./test > torture.log 2>&1
+  date > torture.log
+	valgrind --leak-check=full --show-reachable=yes --num-callers=20 --suppressions=suppressions ./test >> torture.log 2>&1
 done
