69aa0e711ad534c30c062a96e35257c4782760c3
Ian Gulliver <ian@flamingcow.tv>
2015-10-17 15:55:43 +0200

    Start of hogfather code.

--- static/verify.sh
+++ static/verify.sh
@@ -6,11 +6,24 @@ curl \
   --data output_format=json \
   --data output_info=errors \
   --data output_info=warnings \
-  --data language=ECMASCRIPT6 \
+  --data language=ECMASCRIPT5 \
   --data warning_level=verbose \
   --data-urlencode "js_code@cosmopolite.js" \
   http://closure-compiler.appspot.com/compile
 echo
 
-gjslint --strict cosmopolite.js
+curl \
+  --silent \
+  --data compilation_level=ADVANCED_OPTIMIZATIONS \
+  --data output_format=json \
+  --data output_info=errors \
+  --data output_info=warnings \
+  --data language=ECMASCRIPT5 \
+  --data warning_level=verbose \
+  --data externs_url=https://www.cosmopolite.org/externs/cosmopolite.js \
+  --data-urlencode "js_code@hogfather.js" \
+  http://closure-compiler.appspot.com/compile
+echo
+
+gjslint --strict cosmopolite.js hogfather.js
 gjslint --strict --nojsdoc test.js
